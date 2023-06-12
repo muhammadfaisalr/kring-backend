4a2dd10995ee0fc6656c775e3eb8c6eed32b32b0
muhammadfaisalr <muhammadfaisalr17@gmail.com>
2023-06-12 23:20:40 +0700

    Update CreateStoreRequest

--- src/main/java/id/muhammadfaisal/kring/model/request/user/CreateStoreRequest.java
+++ src/main/java/id/muhammadfaisal/kring/model/request/user/CreateStoreRequest.java
@@ -8,4 +8,61 @@ public class CreateStoreRequest {
     private Integer minimumOrder;
     private Integer cityId;
     private String address;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getAvatarImg() {
+        return avatarImg;
+    }
+
+    public void setAvatarImg(String avatarImg) {
+        this.avatarImg = avatarImg;
+    }
+
+    public String getCoverImg() {
+        return coverImg;
+    }
+
+    public void setCoverImg(String coverImg) {
+        this.coverImg = coverImg;
+    }
+
+    public Integer getMinimumOrder() {
+        return minimumOrder;
+    }
+
+    public void setMinimumOrder(Integer minimumOrder) {
+        this.minimumOrder = minimumOrder;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
